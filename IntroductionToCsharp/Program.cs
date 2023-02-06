ef71cd71053e45eea80bc9de0ca3afe08156810b
dpkpradhan17 <dpkpradhan17@gmail.com>
2023-02-06 22:36:06 +0530

    Add project files.

---
+++ IntroductionToCsharp/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Project
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter your First Name");
+        string FirstName = Console.ReadLine();
+
+        Console.WriteLine("Enter your Last Name");
+        string LastName = Console.ReadLine();
+
+        Console.WriteLine("Hello {0} {1}",FirstName,LastName);
+    }
+}
